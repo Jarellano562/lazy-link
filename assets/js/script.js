d1fb77ad7fd1bf4d797781a4e6e9b70f92ae3315
Hayden <haydenkd@protonmail.com>
2021-01-25 20:53:40 -0800

    add qr code generate functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,41 @@
 var history = {};
 
-var getUrl = function(encodedUrl){
-    fetch("https://cleanuri.com/api/v1/shorten", {
-        body: "url=https%3A%2F%2Fgoogle.com%2F",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: "POST",
-        mode: "no-cors"
+longUrl = "https://schlocked.com";
+shortenedUrl = "";
+
+var getUrl = function(longUrl){
+    fetch("https://api-ssl.bitly.com/v4/shorten", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer 13d6ed853cd3dd0709742e537be47fb460d48657",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"long_url": longUrl, "domain": "bit.ly"})
       })
       .then(function(response){
-          console.log(response.json());
+        response = response.json();
+          return response;
+      })
+      .then(function(data){
+        shortenedUrl = JSON.stringify(data.link);
+        console.log(shortenedUrl);
       })
 }
 
-var encodeUrl = function(url){
-    // write url encode function
-    return url;
-}
-
+getUrl(longUrl);
 // read data from localstorage
 // if data exists, import then append to history section
+var loadData = function(id, qrLink, shortLink){
+  // render an item to the history
+}
 
+var saveData = function(id, qrLink, shortLink){
+  // add an item to the list and save to localstorage
+}
+
+var deleteItem = function(id){
+  // delete an item from the list and localstorage
+}
 // accept user input, make request for shortened url
 // make request for qr code
 // append requested data to history as an object
