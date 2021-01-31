4bae0865872d2d231105463d53bb64ca14049551
Jesus <jarellano562@yahoo.com>
2021-01-31 12:31:34 -0800

    adding comments from the google doc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
-var endpoint = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
+//THIS IS FOR THE URL SHORTNER 
+//THIS SHOULD BE THE INPUT FOR 
+//THE URL SHORTER 
 
+var endpoint = document.querySelector ("#url-input");
+
+
+//THIS FUNCTION WILL TAKE THE 
+//VALUE FROM THE INPUT BOX 
+//AND RETURN THE VALUE INTO 
+//THE VARABLE 
 function geturl(){
     var url = document.getElementById("urlinput").value;
     var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
@@ -10,22 +19,33 @@ function geturl(){
             return url;
         }
 }
-
+//CREATED A FUNTION 
+//THEN I CALLED A RANDOM STRING SO
+//THAT IT COULD GIVE IT A VALUE 
 function getrandom() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
+    var random_string = Math.random().toString(32).substring(2, 5) 
+        + 
+                        Math.random().toString(32).substring(2, 5);
+    return random_string()
 }
+//USED THE RANDOM FUNTION
+//THIS IS GOING TO OUTPUT A STRING
+//WITH AN AGRUMENT OF 32 BECAUSE 
+//WE NEED THIS IN ORDER TO MAKE THIS 
+//A PROPER STRING WHICH IS CUT AND ADDED TOGTHER 
+
 
 function genhash(){
     if (window.location.hash == ""){
         window.location.hash = getrandom();
     }
 }
+//THIS FUNCTION IS USED TO CHANGE THE 
+//HASH IN THE LOCATION BAR 
 
+//THIS SHOULD BE SENT TO THE ENDPOINT 
+//WHERE THEY SHOULD STORE THE LONG URL
+// 
 function send_request(url) {
     this.url = url;
     $.ajax({
@@ -36,80 +56,29 @@ function send_request(url) {
         'contentType': 'application/json; charset=utf-8'
 })
 }
-
+//THIS IS THE MAIN FUNCTION THAT WILL 
+//SHORT THE ACTUAL URL...WE NEED TO 
+//FIND A WAY TO STRORE THE URL AND 
+//HAVE THE SHORT URL 
 function shorturl(){
     var longurl = geturl();
     genhash();
     send_request(longurl);
 }
 
+//WE WILL GET THE LONG URL LINK
+//TO THE SHORT URL ENTERED INTO 
+//THE ADDRESS BAR 
 var hashh = window.location.hash.substr(1)
 
 if (window.location.hash != "") {
     $.getJSON(endpoint + "/" + hashh, function (data) {
         data = data["result"];
-
         if (data != null) {
             window.location.href = data;
-        }
-
+        }        
     });
-}
-
-var endpoint = "https://www.jsonstore.io/8ba4fd855086288421f770482e372ccb5a05d906269a34da5884f39eed0418a1";
-
-function geturl(){
-    var url = document.getElementById("urlinput").value;
-    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
-    if(!protocol_ok){
-        newurl = "http://"+url;
-        return newurl;
-        }else{
-            return url;
-        }
-}
-
-function getrandom() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
-}
-
-function genhash(){
-    if (window.location.hash == ""){
-        window.location.hash = getrandom();
-    }
-}
-
-function send_request(url) {
-    this.url = url;
-    $.ajax({
-        'url': endpoint + "/" + window.location.hash.substr(1),
-        'type': 'POST',
-        'data': JSON.stringify(this.url),
-        'dataType': 'json',
-        'contentType': 'application/json; charset=utf-8'
-})
-}
-
-function shorturl(){
-    var longurl = geturl();
-    genhash();
-    send_request(longurl);
-}
-
-var hashh = window.location.hash.substr(1)
-
-if (window.location.hash != "") {
-    $.getJSON(endpoint + "/" + hashh, function (data) {
-        data = data["result"];
-
-        if (data != null) {
-            window.location.href = data;
-        }
-
-    });
+//THIS WILL EXECUTE THE INPUT VALUE 
+//OF THE SHORT URL VALUE WHICH IS GOING 
+//TO BE STORED INTO THE HASHH VALUE 
 }
